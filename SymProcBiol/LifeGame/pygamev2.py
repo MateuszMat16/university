416a13dde67081f988270dd1afeaee82296c1a5a
MateuszMat16 <mateusz.matusewicz16@gmail.com>
2024-07-07 22:19:47 +0200

    SymProcBiol: Cleaning files.
    Signed-off-by: MateuszMat16 <mateusz.matusewicz16@gmail.com>

---
+++ SymProcBiol/LifeGame/pygamev2.py
@@ -0,0 +1,101 @@
+# Imports
+import sys
+import pygame
+
+# Configuration
+pygame.init()
+fps = 60
+fpsClock = pygame.time.Clock()
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+
+font = pygame.font.SysFont('Arial', 40)
+
+objects = []
+
+class Button():
+    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.onclickFunction = onclickFunction
+        self.onePress = onePress
+
+        self.fillColors = {
+            'normal': '#ffffff',
+            'hover': '#666666',
+            'pressed': '#333333',
+        }
+
+        self.buttonSurface = pygame.Surface((self.width, self.height))
+        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
+
+        self.alreadyPressed = False
+
+        objects.append(self)
+
+    def process(self):
+
+        mousePos = pygame.mouse.get_pos()
+        
+        self.buttonSurface.fill(self.fillColors['normal'])
+        if self.buttonRect.collidepoint(mousePos):
+            self.buttonSurface.fill(self.fillColors['hover'])
+
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                self.buttonSurface.fill(self.fillColors['pressed'])
+
+                if self.onePress:
+                    self.onclickFunction()
+                    self.buttonSurface.fill("#111111")
+
+                elif not self.alreadyPressed:
+                    self.onclickFunction()
+                    self.alreadyPressed = True
+
+            else:
+                self.alreadyPressed = False
+
+        self.buttonSurface.blit(self.buttonSurf, [
+            self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
+            self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
+        ])
+        screen.blit(self.buttonSurface, self.buttonRect)
+
+     # check mouseover and clicked conditions
+        clicked = False
+        hover = self.buttonRect.collidepoint(mousePos)
+        if hover and pygame.mouse.get_pressed()[0] == 1:
+            clicked = not clicked
+
+        if clicked:
+            self.buttonSurface.fill("#111111")
+            pygame.draw.rect(screen, "#111111", 2)
+
+        elif hover:
+            pass
+
+
+def myFunction():
+    print("Good")
+
+customButton = Button(30, 250, 200, 50, 'Start game', myFunction)
+customButton = Button(30, 30, 400, 100, 'Button One (Lolz)', myFunction)
+customButton = Button(30, 140, 400, 100, 'Button Two (multiPress)', myFunction, True)
+
+# Game loop.
+while True:
+    screen.fill((20, 20, 20))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    for object in objects:
+        object.process()
+
+    pygame.display.flip()
+    fpsClock.tick(fps)
